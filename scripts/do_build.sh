7443795b6996991ffa6db995fc036cf54840729b
Alex Popiel <alexp@alleninstitute.org>
2017-03-02 13:02:13 -0800

    Fixing yet another silly syntax error...

--- scripts/do_build.sh
+++ scripts/do_build.sh
@@ -3,9 +3,9 @@
 set -ex
 
 scripts/make_bintray_credentials.sh
-[ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false"] && scripts/prepare_build_version.sh
+[ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ] && scripts/prepare_build_version.sh
 sbt clean
 sbt core/lock    +core/test    +core/packagedArtifacts
 sbt http/lock    +http/test    +http/packagedArtifacts
 sbt testkit/lock +testkit/test +testkit/packagedArtifacts
-[ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false"] && scripts/tag_and_push_version.sh && sbt +publish
+[ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ] && scripts/tag_and_push_version.sh && sbt +publish
