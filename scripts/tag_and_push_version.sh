cf213f722c124e92351dea9f4ed47d6348d66055
Alex Popiel <alexp@alleninstitute.org>
2017-07-18 13:32:33 -0700

    Update github token

--- scripts/tag_and_push_version.sh
+++ scripts/tag_and_push_version.sh
@@ -7,7 +7,7 @@ version=`sbt -no-colors -batch 'show version' | tail -1 | sed 's/\[info\][ \t]*/
 git add build.sbt */lock.sbt
 git config --global user.email "nobody@example.com"
 git config --global user.name  "TravisCI automated build"
-git commit -q -m "Version tagging for version $newversion"
+git commit -q -m "Version tagging for version $version"
 git tag v$version
 
 set +x
